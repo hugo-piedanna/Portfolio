89d0885beefc20f95f29387b76a5a5a7462b0556
Piedanna Hugo <hugo.piedanna@etu.iut-tlse3.fr>
2023-12-07 12:08:07 +0100

    Change page title

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = 'Hugo PIEDANNA | Portfolio'; // Remplacez par le titre souhaité
+      return args;
+    });
+  }
 })
